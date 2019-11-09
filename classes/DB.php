814e5d592fc304c45cc0d64deb820748f81c5cd0
Kyle JOHNSTON <kjohnsto@c5r3s1.wethinkcode.co.za>
2019-11-09 11:34:59 +0200

    more files

---
+++ classes/DB.php
@@ -0,0 +1,122 @@
+<?php
+class DB {
+    private static $_instance = null;
+    private $_pdo,
+            $_query,
+            $_error = false,
+            $_results,
+            $_count = 0;
+
+    private function __construct() {
+        try {
+            $this->_pdo = new PDO('mysql:host=' . Config::get('mysql/host') . ';dbname=' . Config::get('mysql/db'), Config::get('mysql/username'), Config::get('mysql/password'));
+            $this->_pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $this->_pdo->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+        } catch(PDOException $err) {
+            die($err->getMessage());
+        }
+    }
+
+    public static function getInstance() {
+        if(!isset(self::$_instance)) {
+            self::$_instance = new DB();
+        }
+        return self::$_instance;
+    }
+
+    public function query($sql, $params = array()) {
+        $this->_error = false;
+        if($this->_query = $this->_pdo->prepare($sql)) {
+            $i = 1;
+            if(count($params)) {
+                foreach($params as $param) {
+                    $this->_query->bindValue($i, $param);
+                    $i++;
+                }
+            }
+
+            if($this->_query->execute()) {
+                $this->_results = $this->_query->fetchAll(PDO::FETCH_OBJ);
+                $this->_count = $this->_query->rowCount();
+            } else {
+                $this->_error = true;
+            }
+        }
+
+        return $this;
+    }
+
+    public function action($action, $table, $where = array()) {
+        if(count($where) === 3) {
+            $operators = array('=', '>', '<', '>=', '<=');
+
+            $field      = $where[0];
+            $operator   = $where[1];
+            $value      = $where[2];
+
+            if(in_array($operator, $operators)) {
+                $sql = "{$action} FROM {$table} WHERE {$field} {$operator} ?";
+
+                if(!$this->query($sql, array($value))->error()) {
+                    return $this;
+                }
+            }
+        }
+        return false;
+    }
+
+    public function get($table, $where) {
+        return $this->action('SELECT *', $table, $where);
+    }
+
+    public function delete($table, $where) {
+        return $this->action('DELETE', $table, $where);
+    }
+
+    public function results() {
+        return $this->_results;
+    }
+
+    public function first() {
+        return $this->results()[0];
+    }
+
+    public function insert($table, $fields = array()) {
+        try {
+            $eh = $this->_pdo->prepare("INSERT INTO `camagru`.`users` (`username`,`password`,`salt`,`name`,`groups`) VALUES (?,?,?,?,1);");
+            $eh->execute($fields);
+            return true;
+        } catch(PDOException $e) {
+            return $e->getMessage() . "\n";
+        }
+    }
+
+    public function update($table, $id, $fields) {
+        $set = '';
+        $x = 1;
+
+        foreach($fields as $name => $value) {
+            $set .= "{$name} = ?";
+            if($x < count($fields)) {
+                $set .= ', ';
+            }
+            $x++;
+        }
+
+        $sql = "UPDATE {$table} SET {$set} WHERE id = {$id}";
+
+        if(!$this->query($sql, $fields)->error()) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public function error() {
+        return $this->_error;
+    }
+
+    public function count() {
+        return $this->_count;
+    }
+}
